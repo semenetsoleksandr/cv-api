7376cdb4fb9f0e70f3bc5bf2c5c164bbf03363d0
Sergii Luchko <sergii.luchko@gmail.com>
2025-02-26 17:21:45 +0100

    added example how to work with classes

--- sql.js
+++ sql.js
@@ -1,61 +1,70 @@
 import sqlite3 from 'sqlite3';
+import { promisify } from 'node:util'
 
-const db = new sqlite3.Database('skills.db', (err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    console.log('connect')
-})
+export class SkillsDb {
+    db;
+    allQuery;
+    runQuery;
+    tableName = 'skillsDB';
 
-// function createTable() {
-//     db.run("CREATE TABLE skillsBD (id INT PRIMARY KEY, skill TEXT)", (err) => {
-//         if (err) {
-//             console.log(err.message);
-//         }
-//         console.log('Create table')
-//     })
-// }
-
-export function addSkillToBD(newId, newsSill) {
-    db.run("INSERT INTO skillsBD (id, skill) VALUES (?,?)", [newId, newsSill], (err) => {
-        if (err) {
-            console.log(err.message)
-        }
-        console.log('add skill')
-    })
-}
-
-export function delIdFromDB(id) {
-    db.run('DELETE FROM skillsBD WHERE id = ?', id, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('del skill');
-    });
-}
-
-
-export function getSkillsFromBD() {
-    return new Promise((resolve, _reject) => {
-        db.all('SELECT * FROM skillsBD', [], (err, rows) => {
+    constructor() {
+        this.db = new sqlite3.Database('skills.db', (err) => {
             if (err) {
                 console.log(err.message);
+            } else {
+                console.log('connect');
             }
-            const i = []
-            rows.forEach((row) => {
-                i.push(row)
-            })
-            resolve(i)
-        })
-    })
+        });
+        this.allQuery = promisify(this.db.all.bind(this.db));
+        this.runQuery = promisify(this.db.run.bind(this.db));
+        this.init();
+    }
+
+    async init() {
+       await this.runQuery(
+          `CREATE TABLE IF NOT EXISTS ${this.tableName} (
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             skill TEXT NOT NULL
+         )`
+      );
+    }
+
+    async getSkills() {
+        try {
+            return await this.allQuery(`SELECT * FROM ${this.tableName}`, []);
+        } catch (err) {
+            console.log(err.message);
+            return [];
+        }
+    }
+
+    async addSkill(skill) {
+        try {
+            // unfortunately we can't use promisify to be able to access this.lastID property in result :(
+            return new Promise((resolve, reject) => {
+                this.db.run(
+                    `INSERT INTO ${this.tableName} (skill) VALUES (?)`,
+                    [skill],
+                    function (err) {
+                        if (err) {
+                            reject(err);
+                        } else {
+                            resolve({ id: this.lastID, skill }); // Return inserted object with ID
+                        }
+                    }
+                );
+            });
+        } catch (err) {
+            console.log(err.message);
+            return null;
+        }
+    }
+
+    async deleteSkill(id) {
+        try {
+            return await this.runQuery(`DELETE FROM ${this.tableName} WHERE id = ?`, [id]);
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
 }
-
-// export function closeBD() {
-//     db.close((err) => {
-//         if (err) {
-//             console.log(err.message)
-//         }
-//         console.log('disconnect from db')
-//     })
-// }
-
