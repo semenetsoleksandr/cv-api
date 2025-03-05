0445c710b78d74adbe455b72a102ab2af779c1c7
Olek Semenets <bobrt440@gmail.com>
2025-03-05 19:21:52 +0100

    add class SkillBD

--- sql.js
+++ sql.js
@@ -1,61 +1,66 @@
 import sqlite3 from 'sqlite3';
+import {promisify} from 'node:util'
 
-const db = new sqlite3.Database('skills.db', (err) => {
-    if (err) {
-        console.log(err.message);
-    }
-    console.log('connect')
-})
+export class SkillsBD {
+    db;
+    tableName = 'skillsBD'
 
-// function createTable() {
-//     db.run("CREATE TABLE skillsBD (id INT PRIMARY KEY, skill TEXT)", (err) => {
-//         if (err) {
-//             console.log(err.message);
-//         }
-//         console.log('Create table')
-//     })
-// }
-
-export function addSkillToBD(newId, newsSill) {
-    db.run("INSERT INTO skillsBD (id, skill) VALUES (?,?)", [newId, newsSill], (err) => {
-        if (err) {
-            console.log(err.message)
-        }
-        console.log('add skill')
-    })
-}
-
-export function delIdFromDB(id) {
-    db.run('DELETE FROM skillsBD WHERE id = ?', id, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('del skill');
-    });
-}
-
-
-export function getSkillsFromBD() {
-    return new Promise((resolve, _reject) => {
-        db.all('SELECT * FROM skillsBD', [], (err, rows) => {
+    constructor(props) {
+        this.db = new sqlite3.Database('skills.db', (err) => {
             if (err) {
                 console.log(err.message);
             }
-            const i = []
-            rows.forEach((row) => {
-                i.push(row)
-            })
-            resolve(i)
+            console.log('connect')
         })
-    })
-}
+        this.init()
+        //this.delTable()
+    }
 
-// export function closeBD() {
-//     db.close((err) => {
-//         if (err) {
-//             console.log(err.message)
-//         }
-//         console.log('disconnect from db')
-//     })
-// }
+
+    async init() {
+        await this.db.run(
+            `CREATE TABLE IF NOT EXISTS skillsBD
+             (
+                 id    INTEGER PRIMARY KEY AUTOINCREMENT,
+                 skill TEXT
+             )`
+        );
+    }
+
+    async delTable() {
+        await this.db.run(
+            `DROP TABLE skillsBD;`
+        );
+    }
+
+    async addSkillToBD(newSkill) {
+        try {
+            const asyncRun = promisify(this.db.run).bind(this.db);
+            await asyncRun('INSERT INTO skillsBD (skill) VALUES (?)', [newSkill])
+            return {id: id.lastID, newSkill}
+        } catch (err) {
+            console.log('ttt')
+        }
+    }
+
+    async delIdFromDB(id) {
+        try {
+            const asyncRun = promisify(this.db.run).bind(this.db)
+            return await asyncRun('DELETE FROM skillsBD WHERE id = ?', [id])
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
+    async getSkillsFromBD() {
+        try {
+            const asyncAll = promisify(this.db.all).bind(this.db)
+            return await asyncAll('SELECT * FROM skillsBD', [])
+        } catch (err) {
+            console.log(err.message)
+        }
+
+    }
+
+}
 
