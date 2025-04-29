7ddb0d268d6d9503a6e0938bc6467ba271dd5e96
Olek Semenets <bobrt440@gmail.com>
2025-04-29 15:51:14 +0200

    Task 2 complete

--- src/ modules/skills/router.ts
+++ src/ modules/skills/router.ts
@@ -2,9 +2,14 @@ import express, {Router} from "express";
 import {createSkillRoute} from "./createSkill";
 import {getSkillRoute} from "./getSkills";
 import {removeSkillRoute} from "./removeSkill";
+import {
+    validateCreateSkillRequest,
+    validateRemoveSkillIRequest
+} from "./validation";
+import handleValidationErrors from "../middlware/handleValidationErrors";
 
 export const skillsRouter = Router()
 
-skillsRouter.get('/',getSkillRoute)
-skillsRouter.post('/',createSkillRoute)
-skillsRouter.delete('/:id',removeSkillRoute)
+skillsRouter.get('/',getSkillRoute);
+skillsRouter.post('/',validateCreateSkillRequest,handleValidationErrors, createSkillRoute)
+skillsRouter.delete('/:id',validateRemoveSkillIRequest,handleValidationErrors,removeSkillRoute)
---
+++ src/ modules/skills/validation.ts
@@ -0,0 +1,9 @@
+import { body, param } from 'express-validator';
+
+export const validateCreateSkillRequest = [
+    body('skill').isString().notEmpty().withMessage('Name is required')
+]
+
+export const validateRemoveSkillIRequest = [
+    param('id').isInt().withMessage('ID must be an integer')
+]
