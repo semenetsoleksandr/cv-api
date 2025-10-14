4a42ec6a8d4f66e6876b8876180b4f851095ac87
Olek Semenets <bobrt440@gmail.com>
2025-10-15 00:30:29 +0200

    fix some errors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,8 @@
 import express from "express";
-import {skillsRouter} from "./modules/skills/router";
+import {skillsRouter} from "../modules/skills/router";
 import cors from "cors";
-import {logRequest} from "./middlware/logger";
-import {messagesRouter} from "./modules/messages/router";
+import {logRequest} from "../middlware/logger";
+import {messagesRouter} from "../modules/messages/router";
 
 const app = express()
 
